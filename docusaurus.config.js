8f5023eceec784a0c4a79ee2df9d50410fc4ccb2
slhome <slhome2010@mail.ru>
2026-02-06 19:13:35 +0500

    V 1.0.0 Removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,10 +76,7 @@ const config = {
             /*  current: {
                label: 'v1.0.0',
                path: '1.0.0',
-             }, */           
-            "1.0.0": {
-              label: 'v1.0.0',             
-            },
+             }, */         
             "3.0.0": {
               label: "v3.0.0",             
             },
